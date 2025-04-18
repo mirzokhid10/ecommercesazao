48f81acd42b17f51939266b2f5e4a3ab3f658595
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2025-04-18 21:28:07 +0500

    day 3 | vid 10 | vendor setup dashboard

---
+++ resources/views/vendor/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="dashboard_sidebar">
+    <span class="close_icon">
+        <i class="far fa-bars dash_bar"></i>
+        <i class="far fa-times dash_close"></i>
+    </span>
+    {{-- {{ asset($logoSetting->logo) }} --}}
+    <a href="javascript:;" class="dash_logo"><img src="{{ asset('frontend/images/logo.png') }}" alt="logo"
+            class="img-fluid"></a>
+    <ul class="dashboard_link">
+        <li><a class="" href="{{ route('user.dashboard') }}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
+        </li>
+        <li><a class="" href="{{ url('/') }}"><i class="fas fa-home"></i>Go To Home Page</a></li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+            this.closest('form').submit();"><i
+                        class="far fa-sign-out-alt"></i> Log out</a>
+            </form>
+        </li>
+    </ul>
+</div>
